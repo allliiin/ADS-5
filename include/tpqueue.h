1f08a809b4647928771cad52a5aeaaa20b661815
allliiin <78855062+allliiin@users.noreply.github.com>
2021-05-30 22:22:35 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,75 +5,68 @@
 
 template<typename T>
 class TPQueue {
+	 private:
+	struct ITEM {
+		T data;
+		ITEM* follow;
+		ITEM* previous;
+	};
+	ITEM* head;
+	ITEM* tail;
+	TPQueue::ITEM* create(const T& data, ITEM*previous) {
+		ITEM* item = new ITEM;
+		item->data = data;
+		item->follow = nullptr;
+		item->previous = previous;
+		return item;
+	}
 
- struct ITEM {
-   T data;
-   ITEM * next 
+	
+	 public:
+	TPQueue() :head(nullptr), tail(nullptr) {}
+	~TPQueue() {
+		while (head)
+		pop();
+	}
+	void push(const T& data) {
+		if (tail && head) {
+			ITEM* current = tail;
+			while (current && data.prior > (current->data).prior) {
+				current = current->previous;
+			}
+			if (current) {
+				ITEM* temp = current->follow;
+				current->follow = create(data, current);
+				current = current->follow;
+				current->follow = temp;
+				if (temp)
+				temp->previous = current;
+				else
+				tail = current;
+			} else {
+				current = create(data, nullptr);
+				current->follow = head;
+				head->previous = current;
+				head = current;
+			}
+		} else {
+			head = create(data, nullptr);
+			tail = head;
+		}
+	}
+	T pop() {
+		assert(head);
+		ITEM* temp = head->follow;
+		T data = head->data;
+		if (temp)
+		temp->previous = nullptr;
+		delete head;
+		head = temp;
+		return data;
+	}
 };
- public:
-   TPQueue() : head(nullptr), tail(nullptr) {}
-   ~TPQueue();
-   void push(const T&);
-   T pop();
- private:
-   TPQueue :: ITEM * create (const T&);
-   ITEM * head;
-   ITEM * tail;
-};
-template < typename T >
-typename TPQueue <T>:: ITEM * TPQueue  <T>:: create (const T& data) {
-ITEM * item = new ITEM;
-item−>data = data;
-item−>next = nullptr;
-return item;
-}
-template < typename T >
-TPQueue <T>:: TPQueue (const T& data) {
-   head = create (data);
-   tail = head;
-}
-template < typename T >
-     TPQueue <T>::~TPQueue() {
-        while (head)
-           pop();
-     }
-template < typename T >
-void TPQueue  <T>:: push (const T& data) {
-  if (tail && head) {
-    ITEM* temp = head;
-   if (data.prior >= temp->data.prior) {
-    temp = create(data);
-    temp-next = head;
-    head = temp;
-   }
-  } else
-    while (data.prior <= temp->data.prior) && (temp->next)
-      temp = temp->next;
-  if (!temp->next) {
-    temp = create(data);
-    tail->next = temp;
-    tail = temp;
-  } else {
-    ITEM* tempI = temp;
-    temp = create(data);
-    temp->next = tempI;
-  }
-}
-template < typename T >
-T TPQueue <T>:: pop () {
-   if (head) {
-        ITEM * temp = head−>next;
-        T data = head−>data;
-        delete head;
-        head = temp;
-        return data;
-   }
-
-};
-
 struct SYM {
   char ch;
   int  prior;
 };
-
 #endif // INCLUDE_TPQUEUE_H_
