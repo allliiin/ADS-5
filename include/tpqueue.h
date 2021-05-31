e803c154d9fe2734730db1ef86a8033108f89369
allliiin <78855062+allliiin@users.noreply.github.com>
2021-05-31 10:55:14 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,68 +5,69 @@
 
 template<typename T>
 class TPQueue {
-	 private:
-	struct ITEM {
-		T data;
-		ITEM* follow;
-		ITEM* previous;
-	};
-	ITEM* head;
-	ITEM* tail;
-	TPQueue::ITEM* create(const T& data, ITEM*previous) {
-		ITEM* item = new ITEM;
-		item->data = data;
-		item->follow = nullptr;
-		item->previous = previous;
-		return item;
-	}
-
-	
-	 public:
-	TPQueue() :head(nullptr), tail(nullptr) {}
-	~TPQueue() {
-		while (head)
-		pop();
-	}
-	void push(const T& data) {
-		if (tail && head) {
-			ITEM* current = tail;
-			while (current && data.prior > (current->data).prior) {
-				current = current->previous;
-			}
-			if (current) {
-				ITEM* temp = current->follow;
-				current->follow = create(data, current);
-				current = current->follow;
-				current->follow = temp;
-				if (temp)
-				temp->previous = current;
-				else
-				tail = current;
-			} else {
-				current = create(data, nullptr);
-				current->follow = head;
-				head->previous = current;
-				head = current;
-			}
-		} else {
-			head = create(data, nullptr);
-			tail = head;
-		}
-	}
-	T pop() {
-		assert(head);
-		ITEM* temp = head->follow;
-		T data = head->data;
-		if (temp)
-		temp->previous = nullptr;
-		delete head;
-		head = temp;
-		return data;
-	}
+ private:
+struct ITEM {
+T data;
+ITEM* follow;
+ITEM* previous;
 };
+ITEM* head;
+ITEM* tail;
+TPQueue::ITEM* create(const T& data, ITEM*previous) {
+ITEM* item = new ITEM;
+item->data = data;
+item->follow = nullptr;
+item->previous = previous;
+return item;
+}
+	
+ public:
+TPQueue() :head(nullptr), tail(nullptr) {}
+~TPQueue() {
+while (head)
+pop();
+}
+void push(const T& data) {
+if (tail && head) {
+ITEM* current = tail;
+while (current && data.prior > (current->data).prior) {
+current = current->previous;
+}
+if (current) {
+ITEM* temp = current->follow;
+current->follow = create(data, current);
+current = current->follow;
+current->follow = temp;
+if (temp)
+temp->previous = current;
+else
+tail = current;
+} else {
+current = create(data, nullptr);
+current->follow = head;
+head->previous = current;
+head = current;
+}
+} else {
+head = create(data, nullptr);
+tail = head;
+}
+}
+T pop() {
+assert(head);
+ITEM* temp = head->follow;
+T data = head->data;
+if (temp)
+temp->previous = nullptr;
+delete head;
+head = temp;
+return data;
+}
+};
+
 struct SYM {
   char ch;
   int  prior;
 };
+
 #endif // INCLUDE_TPQUEUE_H_
